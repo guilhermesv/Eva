cae4e8b0fde71372e25bf2d61d3daf25abc18bc2
Guilherme Vieira <guilhermesv@gmail.com>
2021-07-18 11:55:43 -0300

    Limpeza

--- functions.php
+++ functions.php
@@ -71,18 +71,6 @@ if ( ! function_exists( 'eva_setup' ) ) :
 			)
 		);
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'eva_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
-
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
@@ -150,11 +138,6 @@ function eva_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'eva_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
